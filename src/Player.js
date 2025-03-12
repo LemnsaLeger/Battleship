03b497a294ba3cf8d85eb4fe18754e540a8cc016
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-03-12 16:36:29 -0700

    Update file, Make Computer Target Adjacent Cells If Hit One

--- src/Player.js
+++ src/Player.js
@@ -1,30 +1,50 @@
 import { Gameboard } from "./Gameboard.js";
-
- export class Player {
+export class Player {
   constructor(isHuman = true) {
-    this.gameboard = new Gameboard(); // each player has a gameboard
-    this.isHuman = isHuman; 
+    this.gameboard = new Gameboard();
+    this.isHuman = isHuman;
     this.previousAttacks = [];
+    this.lastHit = null; // Track the last hit
   }
 
   attack(opponentGameboard, coordinates) {
-    if(!this.isHuman) {
-      const pos = this._randomCoordinates();
-      opponentGameboard.receiveAttack(pos);
-    } else {
+    if (this.isHuman) {
       opponentGameboard.receiveAttack(coordinates);
+    } else {
+      // Computer player: target adjacent cells after a hit
+      let x, y;
+      if (this.lastHit) {
+        // Try adjacent cells
+        const [lastX, lastY] = this.lastHit;
+        const directions = [
+          [lastX + 1, lastY],
+          [lastX - 1, lastY],
+          [lastX, lastY + 1],
+          [lastX, lastY - 1],
+        ];
+        const validDirections = directions.filter(
+          ([x, y]) => x >= 0 && x < 10 && y >= 0 && y < 10
+        );
+        const [targetX, targetY] =
+          validDirections[Math.floor(Math.random() * validDirections.length)];
+        x = targetX;
+        y = targetY;
+      } else {
+        // Random attack
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+      }
+
+      // Record the attack
+      this.previousAttacks.push([x, y]);
+      opponentGameboard.receiveAttack([x, y]);
+
+      // Update lastHit if the attack was successful
+      if (opponentGameboard.board[x + y * 10] === "hit") {
+        this.lastHit = [x, y];
+      } else {
+        this.lastHit = null;
+      }
     }
   }
-
-  _randomCoordinates() {
-    let x, y;
-    do {
-      x = Math.floor(Math.random() * 10); // random number 0 - 9
-      y = Math.floor(Math.random() * 10);
-    } while (this.previousAttacks.some(attack => attack[0] === x && attack[1] === y));
-
-    this.previousAttacks.push([x, y]);
-    return [x, y];
-  }
 }
-
